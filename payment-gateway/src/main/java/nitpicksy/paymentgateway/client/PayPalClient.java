54c9cbf7a77b76604667e32f7f6adef1de405f17
dusan <nikolic.dusan.dey@gmail.com>
2020-12-01 16:29:49 +0100

    zuul filter, feign routes

--- payment-gateway/src/main/java/nitpicksy/paymentgateway/client/PayPalClient.java
+++ payment-gateway/src/main/java/nitpicksy/paymentgateway/client/PayPalClient.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "paypal-service")
+@FeignClient(name = "zuul")
 public interface PayPalClient {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/paypal-test/health")
+    @RequestMapping(method = RequestMethod.GET, path = "paypal-service/paypal-test/health")
     String healthCheck();
 
 }
--- zuul-api-gateway/src/main/java/nitpicksy/zuulapigateway/ZuulApiGatewayApplication.java
+++ zuul-api-gateway/src/main/java/nitpicksy/zuulapigateway/ZuulApiGatewayApplication.java
@@ -1,14 +1,21 @@
 package nitpicksy.zuulapigateway;
 
+import nitpicksy.zuulapigateway.filters.ZuulPreFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulApiGatewayApplication {
+
+	@Bean
+	public ZuulPreFilter simpleFilter() {
+		return new ZuulPreFilter();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApiGatewayApplication.class, args);
