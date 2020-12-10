6a0747ec048054743cbe46ce88ac310f71809779
dusan <nikolic.dusan.dey@gmail.com>
2020-12-10 01:11:46 +0100

    activate account page

--- literary-society-frontend/src/components/Authentication/ActivateAccount/ActivateAccount.js
+++ literary-society-frontend/src/components/Authentication/ActivateAccount/ActivateAccount.js
@@ -1,3 +1,23 @@
-const ActivateAccount = (props) => {};
+import { Avatar, Container, CssBaseline, Typography } from "@material-ui/core";
+import { useStyles } from "./ActivateAccountStyles";
+import DoneAllRoundedIcon from "@material-ui/icons/DoneAllRounded";
+import React from "react";
+
+const ActivateAccount = (props) => {
+  const classes = useStyles();
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <DoneAllRoundedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h4">
+          You're all set!
+        </Typography>
+      </div>
+    </Container>
+  );
+};
 
 export default ActivateAccount;
