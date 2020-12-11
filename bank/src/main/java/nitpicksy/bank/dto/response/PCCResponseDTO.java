b7a4df9af5cce21bfe1e84bb71fcfef670c14853
JelenaPopov <popov.jelena2@gmail.com>
2020-12-11 20:01:10 +0100

    change dto object

--- bank/src/main/java/nitpicksy/bank/dto/response/PCCResponseDTO.java
+++ bank/src/main/java/nitpicksy/bank/dto/response/PCCResponseDTO.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import nitpicksy.bank.enumeration.TransactionStatus;
-
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Positive;
 import java.sql.Timestamp;
 
