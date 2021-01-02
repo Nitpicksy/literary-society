7eb514ceb66eadb27e8e34e143578a193d20a449
dusan <nikolic.dusan.dey@gmail.com>
2021-01-02 01:07:23 +0100

    diagram logic

--- literary-society/src/main/java/nitpicksy/literarysociety/camunda/service/CommitteeVotingService.java
+++ literary-society/src/main/java/nitpicksy/literarysociety/camunda/service/CommitteeVotingService.java
@@ -1,38 +1,60 @@
 package nitpicksy.literarysociety.camunda.service;
 
-import nitpicksy.literarysociety.dto.request.FormSubmissionDTO;
+import nitpicksy.literarysociety.enumeration.CommitteeMemberOpinion;
+import nitpicksy.literarysociety.model.OpinionOfCommitteeMember;
+import nitpicksy.literarysociety.model.Writer;
 import nitpicksy.literarysociety.repository.CommitteeOpinionRepository;
+import nitpicksy.literarysociety.repository.WriterRepository;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @Service
 public class CommitteeVotingService implements JavaDelegate {
 
     private CommitteeOpinionRepository committeeOpinionRepository;
+    private WriterRepository writerRepository;
 
     @Override
     public void execute(DelegateExecution execution) throws Exception {
-        List<FormSubmissionDTO> formData = (List<FormSubmissionDTO>) execution.getVariable("formData");
-        Map<String, String> map = formData.stream().collect(Collectors.toMap(FormSubmissionDTO::getFieldId, FormSubmissionDTO::getFieldValue));
 
-        String writer = (String) execution.getVariable("writer");
-        System.out.println("Writer" + writer);
-//        Book book = bookRepository.findOneById(bookId);
-//        book.setStatus(BookStatus.valueOf(map.get("decision")));
-//        BookStatus bookStatus = book.getStatus();
-//        bookRepository.save(book);
+        String username = (String) execution.getVariable("writer");
+        Writer writer = writerRepository.findByUsername(username);
+        execution.setVariable("attempts", writer.getAttempts());
+
+        List<OpinionOfCommitteeMember> opinionOfCommitteeMembers = committeeOpinionRepository.findOpinionOfCommitteeMemberByWriterUsername(username);
+
+        execution.setVariable("moreDocuments", false);
+        execution.setVariable("rejected", false);
+
+        opinionOfCommitteeMembers.forEach(opinion -> {
+            if (opinion.getOpinion().equals(CommitteeMemberOpinion.MORE_DOCUMENTS)) {
+                execution.setVariable("moreDocuments", true);
+            }
+        });
+
+        if (isRejected(opinionOfCommitteeMembers)) {
+            execution.setVariable("rejected", true);
+        }
+    }
+
+    private Boolean isRejected(List<OpinionOfCommitteeMember> opinionOfCommitteeMembers) {
+        int counter = 0;
+
+        for (OpinionOfCommitteeMember opinionOfCommitteeMember : opinionOfCommitteeMembers) {
+            if (opinionOfCommitteeMember.getOpinion().equals(CommitteeMemberOpinion.DOES_NOT_MEETS_CRITERIA)) {
+                counter++;
+            }
+        }
+        return counter >= Math.round((double) opinionOfCommitteeMembers.size() / 2);
     }
 
     @Autowired
-    public CommitteeVotingService(CommitteeOpinionRepository committeeOpinionRepository) {
+    public CommitteeVotingService(CommitteeOpinionRepository committeeOpinionRepository, WriterRepository writerRepository) {
         this.committeeOpinionRepository = committeeOpinionRepository;
+        this.writerRepository = writerRepository;
     }
-
-
 }
