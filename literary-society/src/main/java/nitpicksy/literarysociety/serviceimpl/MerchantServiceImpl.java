ecabca89da37cf670175cd98ffe1a6a4b079d6f9
JelenaPopov <popov.jelena2@gmail.com>
2021-01-05 17:23:55 +0100

    change send email to merchant

--- literary-society/src/main/java/nitpicksy/literarysociety/serviceimpl/MerchantServiceImpl.java
+++ literary-society/src/main/java/nitpicksy/literarysociety/serviceimpl/MerchantServiceImpl.java
@@ -117,6 +117,11 @@ public class MerchantServiceImpl implements MerchantService {
         String subject = "Request to register accepted";
         StringBuilder sb = new StringBuilder();
         sb.append("Your request to register is accepted by a Literary Society administrator.");
+        sb.append("You have to support all available payment methods before you start to sell books.");
+        sb.append("To support all available payment methods click the following link:");
+        sb.append(System.lineSeparator());
+        sb.append(getLocalhostURL());
+        sb.append(String.format("payment-data"));
         sb.append(System.lineSeparator());
         String text = sb.toString();
         emailNotificationService.sendEmail(recipientEmail, subject, text);
